31f5e0b7ee6e7b2e18cff11bdfc7b048fb489770
EndlessMissery <r.mnacek@seznam.cz>
2024-03-08 22:16:35 +0100

    Update

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -4,27 +4,32 @@ import TabButton from "./components/TabButton/TabButton.jsx";
 import DeveloperPortfolio from "./components/DeveloperPortfolio/DeveloperPortfolio.jsx";
 import ContactInfo from "./components/ContactInfo/ContactInfo.jsx";
 import AboutMe from "./components/AboutMe/AboutMe.jsx";
-import "./styles/home.css";
+import { EXAMPLES } from "./data.js";
+import "./components/Home/Home.css";
 import "./components/DeveloperPortfolio/DeveloperPortfolio.css";
 import "./components/Designs/Designs.css";
 import "./components/AboutMe/AboutMe.css";
 import "./components/ContactInfo/ContactInfo.css";
-import { EXAMPLES } from "./data.js";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  const [selectedTopic, setSelectedTopic] = useState(() => localStorage.getItem("selectedTopic"));
 
   useEffect(() => {
-    const storedSelectedTopic = localStorage.getItem("selectedTopic");
-    if (storedSelectedTopic) {
-      setSelectedTopic(storedSelectedTopic);
-    }
-  }, []);
+    if (selectedTopic) localStorage.setItem("selectedTopic", selectedTopic);
+  }, [selectedTopic]);
 
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-    localStorage.setItem("selectedTopic", selectedButton);
-  }
+  const handleSelect = (selectedButton) => setSelectedTopic(selectedButton);
+
+  const renderContent = () => {
+    switch (selectedTopic) {
+      case "portfolio":
+        return <DeveloperPortfolio projects={EXAMPLES.portfolio.projects} />;
+      case "contact":
+        return <ContactInfo />;
+      default:
+        return <AboutMe selectedTopic={selectedTopic} />;
+    }
+  };
 
   return (
     <div>
@@ -32,59 +37,18 @@ function App() {
       <div className="verticalline"></div>
       <section id="examples">
         <menu>
-          <TabButton
-            isSelected={selectedTopic === "home"}
-            onSelect={() => handleSelect("home")}
-          >
-            Home
-          </TabButton>
-          <br />
-          <TabButton
-            isSelected={selectedTopic === "portfolio"}
-            onSelect={() => handleSelect("portfolio")}
-          >
-            Developer Portfolio
-          </TabButton>
-          <br />
-          <TabButton
-            isSelected={selectedTopic === "design"}
-            onSelect={() => handleSelect("design")}
-          >
-            Designs
-          </TabButton>
-          <br />
-          <TabButton
-            isSelected={selectedTopic === "about"}
-            onSelect={() => handleSelect("about")}
-          >
-            About Me
-          </TabButton>
-          <br />
-          <TabButton
-            isSelected={selectedTopic === "info"}
-            onSelect={() => handleSelect("info")}
-          >
-            Info
-          </TabButton>
-          <br />
-          <TabButton
-            isSelected={selectedTopic === "contact"}
-            onSelect={() => handleSelect("contact")}
-          >
-            Contact
-          </TabButton>
-          </menu>
-        {selectedTopic && (
-          <div id="tab-content">
-            {selectedTopic === "portfolio" && (
-              <DeveloperPortfolio projects={EXAMPLES.portfolio.projects} />
-            )}
-            {selectedTopic === "contact" && <ContactInfo />}
-            {(selectedTopic !== "portfolio" && selectedTopic !== "contact") && (
-              <AboutMe selectedTopic={selectedTopic} />
-            )}
-          </div>
-        )}
+          {["home", "portfolio", "design", "about", "info", "contact"].map((topic) => (
+            <React.Fragment key={topic}>
+              <TabButton
+                isSelected={selectedTopic === topic}
+                onSelect={() => handleSelect(topic)}>
+                {topic.charAt(0).toUpperCase() + topic.slice(1)}
+              </TabButton>
+              <br />
+            </React.Fragment>
+          ))}
+        </menu>
+        {selectedTopic && <div id="tab-content">{renderContent()}</div>}
       </section>
     </div>
   );
