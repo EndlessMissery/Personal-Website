d38985851b53d0aaf7ea3433af3f06d87a9d57b3
EndlessMissery <r.mnacek@seznam.cz>
2024-04-01 21:31:09 +0200

    up

--- website/src/components/DeveloperPortfolio/portfolio.js
+++ website/src/components/DeveloperPortfolio/portfolio.js
@@ -5,37 +5,37 @@ portfolio: {
       {
         difficulty: "Beginner",
         title: "BMI Calculator",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        technologies: ["HTML", "CSS", "JS"],
         link: "https://github.com/EndlessMissery/BMI-Calculator-VanillaJS-",
       },
       {
         difficulty: "Beginner",
-        title: "DiceChallenge",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        title: "Dice Challenge",
+        technologies: ["HTML", "CSS", "JS"],
         link: "https://github.com/EndlessMissery/Dice-game-VanillaJS-",
       },
       {
         difficulty: "Beginner",
         title: "Calculator",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        technologies: ["HTML", "CSS", "JS"],
         link: "https://github.com/EndlessMissery/Calculator-VanillaJS-",
       },
       {
         difficulty: "Beginner",
-        title: "ToDoList",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        title: "To Do List",
+        technologies: ["HTML", "CSS", "JS"],
         link: "https://github.com/EndlessMissery/To-do-list-VanillaJS-",
       },
       {
         difficulty: "Beginner",
         title: "Snake",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        technologies: ["HTML", "CSS", "JS"],
         link: "https://github.com/EndlessMissery/Snake-VanillaJS-",
       },
       {
         difficulty: "Beginner",
-        title: "WeatherApp",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        title: "Weather App",
+        technologies: ["HTML", "CSS", "JS"],
         link: "https://github.com/EndlessMissery/Weather-VanillaJS-",
       },
       {
@@ -51,9 +51,15 @@ portfolio: {
         link: "https://github.com/EndlessMissery/Personal-Website",
       },
       {
+        difficulty: "Intermediate",
+        title: "Typing Test",
+        technologies: ["HTML", "CSS", "JS"],
+        link: "https://github.com/EndlessMissery/Typing-Test",
+      },
+      {
         difficulty: "Expert",
         title: "TBD",
-        technologies: ["HTML", "CSS", "JavaScript"],
+        technologies: ["HTML", "CSS", "ReactJS"],
         link: "TBD",
         }
     ]
